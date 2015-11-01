5d00ae095c8b4abb41cfc371463b79e996e3429f
Elegant Lai <elegant.lai@kiwifi.cn>
2015-11-01 16:51:57 +0800

    用TS初步重写Observer模式

---
+++ ts/observer.js
@@ -0,0 +1,19 @@
+///<reference path="ObserverSubject.ts"/>
+var controlCheckbox = document.getElementById("mainCheckbox"), addBtn = document.getElementById("btn_add"), container = document.getElementById("observers");
+var AddNewObserver = function () {
+    var check = document.createElement("input"), label = document.createElement("label"), text = document.createTextNode("New Checkbox");
+    check.type = "checkbox";
+    ObserverSubject.extend(new ObserverSubject.Observer(), check);
+    check.Update = function (value) {
+        this.checked = value;
+    };
+    controlCheckbox.AddObserver(check);
+    label.appendChild(check);
+    label.appendChild(text);
+    container.appendChild(label);
+};
+ObserverSubject.extend(new ObserverSubject.Subject(), controlCheckbox);
+controlCheckbox.onclick = function () {
+    this.Notify(controlCheckbox.checked);
+};
+addBtn.onclick = AddNewObserver;
