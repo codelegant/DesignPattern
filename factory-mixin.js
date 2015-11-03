ddd4770fccf713e8f20ec7696184105ece70eaef
Elegant Lai <elegant.lai@kiwifi.cn>
2015-11-03 14:16:11 +0800

    明天为何使用call()方法。

--- factory-mixin.js
+++ factory-mixin.js
@@ -7,10 +7,10 @@ var Person = function(firstName, lastName) {
 var Superhero = function(firstName, lastName, powers) {
 	/**
 	 * Person(firstName,lastName);
-	 * 不能用上面这种方式，而是要强制将this传入，否则无法继承Person中的成员
-	 * 如果使用上面的那种方式，this是否改变？明天测试
+	 * 以上的方式中，this为Window(全局)，所以需要手动指定this为Superhero
 	 * https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/create
 	 * https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Function/call
+	 * 在一个子构造函数中，你可以通过调用父构造函数的 call 方法来实现继承
 	 */
 	Person.call(this, firstName, lastName);
 	this.powers = powers;
@@ -18,5 +18,6 @@ var Superhero = function(firstName, lastName, powers) {
 //显式的指定继承原型
 Superhero.prototype=Object.create(Person.prototype);
 var superman=new Superhero("Clark","Kent",["flight","heat-vision"]);
+console.log(Object.create(Person.prototype));
 console.log(superman instanceof Person);
 console.log(superman);
